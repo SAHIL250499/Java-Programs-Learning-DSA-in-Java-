4ec5d0a503b6a629f928cf04fba772add139f454
SAHIL250499 <samp9270@gmail.com>
2023-04-04 19:32:32 +0530

    commit

--- readme.js
+++ readme.js
@@ -21,13 +21,68 @@ const getDirectories = (path, exclude = []) => {
 };
 
 const catDirArray = getDirectories(ROOT_DIR, exclude);
-let content="";
 
 for(let i=0;i<catDirArray.length;i++){
   const item=catDirArray[i];
-  content+=`### [${item}](${GITHUB_REPO_BLOB_URL}${item})\n`
-  content+="\n\n----\n";
+  const superitem={
+        item:item,
+        levels:{},
+  };
+  const levelDirArray=getDirectories(ROOT_DIR+"/"+item,exclude);
+
+  for(let j=0;j< levelDirArray.length;j++){
+    const level=levelDirArray[j];
+    const files=getDirectories(ROOT_DIR+'/'+item+'/'+level,exclude);
+    superitem['levels'][level] =files;
+  }
+  index.push(item);
 }
 
+let Pipe='|';
 
-fs.writeFileSync('README.md', content);
+for(let i=0;i<index.length;i++){
+    let item=superitem[i].item;
+    Pipe+=`[${item}](${GITHUB_REPO_BLOB_URL}/# ${item.toLowerCase()}) |`;
+}
+let content=Pipe+'\n\n----\n';
+
+for(let i=0;i<index.length;i++){
+    const item=index[i];
+    content += `### [${item.item}](${GITHUB_REPO_BLOB_URL}${item.item})\n`;
+
+    const levels=Object.keys(item.levels);
+
+    for (let j=0;j<levels.length;j++){
+        if(item.levels[levels[j]]){
+            content+=`- [${levels[j]}](${GITHUB_REPO_BLOB_URL}${item.item}/${levels[j]})\n`;
+
+            if(levels[j]==='Easy'){
+                easy+=item.levels[levels[j]].length;
+            }
+            else if(levels[j] === "Medium"){
+                medium+=item.levels[levels[j]].length;
+            }
+            else{
+                hard+=item.levels[levels[j]].length;
+            }
+
+            for(let l=0;l<item.levels[levels[j]].length;l++){
+                const file=item.levels[levels[j]][l];
+                content+=`\t - [${file}](${GITHUB_REPO_BLOB_URL}${item.item}/${levels[j]}/${file})\n`;
+                count++;
+            }
+            content+='\n';
+
+        }
+    }
+
+    content+="---\n";
+
+}
+
+let prepend=`Total Problems Solved - ${count}\n\nEasy - ${easy},Medium - ${medium},Hard - ${hard}\n\nCategories\n\n`;
+
+
+
+
+fs.writeFileSync('README.md',prepend+content);
